4b832d8a64f253357f17ac7cbb83cfb402e49157
IshwaryaVetrivel <125111997+IshwaryaVetrivel@users.noreply.github.com>
2023-02-13 13:03:48 +0530

    patient files

--- forms.py
+++ forms.py
@@ -2,21 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from . import models
 
-
-class DonorUserForm(forms.ModelForm):
+#login
+class PatientUserForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['first_name','last_name','username','password']
         widgets = {
         'password': forms.PasswordInput()
         }
-
-class DonorForm(forms.ModelForm):
+#signup
+class PatientForm(forms.ModelForm):
+    
     class Meta:
-        model=models.Donor
-        fields=['bloodgroup','address','mobile','profile_pic']
-
-class DonationForm(forms.ModelForm):
-    class Meta:
-        model=models.BloodDonate
-        fields=['age','bloodgroup','disease','unit']
+        model=models.Patient
+        fields=['age','bloodgroup','disease','address','doctorname','mobile','profile_pic']
